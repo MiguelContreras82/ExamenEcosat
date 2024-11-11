2a3936f6fb94b8e8c435df70071ecc19fc0023e6
MiguelC93 <miguelcontreras82@yahoo.com>
2024-11-11 08:35:41 -0600

    -Se modifican librerias de bakend de ventas.

--- ExamenPython/PythonScripts/leerMarcas.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def leerMarcas ():
-    r = requests.post('http://201.174.8.28:17505/api/devices/v1/mysql/brands', json={"id": 0})
-    print(r.json())
-        
-leerMarcas()
--- ExamenPython/PythonScripts/stringToArray.py
+++
@@ -1,6 +0,0 @@
-def stringToArr():
-    print("Digite la cadena de texto")
-    texto = input()
-    print(list(texto)) 
-
-stringToArr()
